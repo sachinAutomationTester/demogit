209f1c593a33add46d5e64190eb9d02eeef8ae32
sachinAutomationTester <160535851+sachinAutomationTester@users.noreply.github.com>
2024-03-09 23:35:42 +0530

    Update testRunner.java

--- demogit/src/Runner/testRunner.java
+++ demogit/src/Runner/testRunner.java
@@ -9,6 +9,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:\\Users\\SACHIN R\\eclipse-workspace1\\FreecrmBDDFramework\\src\\main\\java\\Features",
         glue={"StepDefinitions"},
+		 publish=true,
 		format= {"pretty","html:test-outout"}
         )
 
